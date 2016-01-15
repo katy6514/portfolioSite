06cdf58f978bd7cfe607a5c1ee0d526054f1d9bf
Katherine Wyman <katherine.i.wyman@gmail.com>
2016-01-15 12:51:16 -0800

    resume and home page work

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!doctype html>
 <html class="no-js" lang="">
 <head>
+	 <!--[if lt IE 9]>
+		  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	 <![endif]-->
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
@@ -30,40 +33,52 @@
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 </head>
 <body>
-  <aside class="sidebar-left">
+    <aside class="sidebar-left">
     <a class="company-logo" href="#">KIW</a>
     <div class="social-links-top">
-      <a href="#"><i class="fa fa-linkedin-square"></i></a>
-      <a href="#"><i class="fa fa-github-square"></i></a>
-      <a href="#"><i class="fa fa-twitter-square"></i></a>
+    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+    <a href="#"><i class="fa fa-github-square"></i></a>
+    <a href="#"><i class="fa fa-twitter-square"></i></a>
     </div>
     <div class="sidebar-links">
-      <a class="link-blue selected" href="./index.php"><i class="fa fa-home"></i>Home</a>
-      <a class="link-red" href="./resume.php"><i class="fa fa-file-text-o"></i>Resume</a>
-      <a class="link-yellow " href="./portfolio.php"><i class="fa fa-folder-o"></i>Projects</a>
-      <a class="link-green" href="./contact.php"><i class="fa fa-envelope-o"></i>Contact</a>
+    <a class="link-blue selected" href="./index.php"><i class="fa fa-home"></i>Home</a>
+    <a class="link-red" href="./resume.php"><i class="fa fa-file-text-o"></i>Resume</a>
+    <a class="link-yellow " href="./portfolio.php"><i class="fa fa-folder-o"></i>Projects</a>
+    <a class="link-green" href="./contact.php"><i class="fa fa-envelope-o"></i>Contact</a>
     </div>
     <div class="social-links-bottom">
-      <a href="#"><i class="fa fa-linkedin-square"></i></a>
-      <a href="#"><i class="fa fa-github-square"></i></a>
-      <a href="#"><i class="fa fa-twitter-square"></i></a>
+    <a href="#"><i class="fa fa-linkedin-square"></i></a>
+    <a href="#"><i class="fa fa-github-square"></i></a>
+    <a href="#"><i class="fa fa-twitter-square"></i></a>
     </div>
-  </aside>
+    </aside>
 
-  <main id="home-page" class="main-content">
-    <div class="section group">
-      <div class="col span_2_of_3">
-        <h1>Howdy!</h1>
-        <p>I'm Katy, I make websites.</p>
-        <p>Ostensibly an astronomer, I currently work at the Center for Astrophysics supporting the Chandra Space Telescope. Over the years my interests and goals have driven me more towards web development and I find myself enjoying making easy to use and nice-looking sites, regardless of viewport size.</p>
-        <p>I'm attempting a thru-hike of the PCT during the summer of 2016, apologies if I appear unresponsive! I might just be a little lost in the woods.</p>
-      </div>
-      <div class="col span_1_of_3">
-        <p id="profile_pic"><img src="./img/yellow_sweater_cwicket.JPG" /></p>
-        <p class="img_caption">Me and a tiny monster/dog: Cricket</p>
-      </div>
-    </div>
-  </main>
+    <main id="home-page" class="main-content">
+        <div class="section group">
+            <div class="col span_2_of_3">
+                <h1>Howdy!</h1>
+                <p>I'm Katy, a full-stack web developer.</p>
+
+                <p>In a previous life I was an astronomer working for the <a href="http://chandra.si.edu/" target="_blank">Chandra Space Telescope</a>. Now I find myself enjoying making easy to use and nice-looking sites, regardless of viewport size.</p>
+
+                <p>I'm currently teaching with the <a href="http://thewc.co/" target="_blank">Women's Coding Collective</a>, a Boston-based initiative commited to helping women succeed in web development.</p>
+
+                <p>I'll be attempting a thru-hike of the <a href="http://www.pcta.org/" target="_blank">PCT</a> during the summer of 2016, apologies if I appear unresponsive! I might just be a little lost in the woods.</p>
+
+            </div>
+            <div class="col span_1_of_3">
+                <p id="profile_pic"><img src="./img/yellow_sweater_cwicket.JPG" /></p>
+                <p class="img_caption">Me and a tiny monster/dog: Cricket</p>
+            </div>
+        </div>
+        <div class="section group">
+            <div class="col span_3_of_3 summary">
+                <h3>At a glance...</h3>
+
+
+            </div>
+        </div>
+    </main>
 
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="js/plugins.js"></script>
--- resume.php
+++ resume.php
@@ -30,168 +30,199 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 </head>
 <body>
-  <aside class="sidebar-left">
-    <a class="company-logo" href="#">KIW</a>
-    <div class="social-links-top">
-      <a href="#"><i class="fa fa-linkedin-square"></i></a>
-      <a href="#"><i class="fa fa-github-square"></i></a>
-      <a href="#"><i class="fa fa-twitter-square"></i></a>
-    </div>
-    <div class="sidebar-links">
-      <a class="link-blue " href="./index.php"><i class="fa fa-home"></i>Home</a>
-      <a class="link-red selected" href="./resume.php"><i class="fa fa-file-text-o"></i>Resume</a>
-      <a class="link-yellow " href="./portfolio.php"><i class="fa fa-folder-o"></i>Projects</a>
-      <a class="link-green" href="./contact.php"><i class="fa fa-envelope-o"></i>Contact</a>
-    </div>
-    <div class="social-links-bottom">
-      <a href="#"><i class="fa fa-linkedin-square"></i></a>
-      <a href="#"><i class="fa fa-github-square"></i></a>
-      <a href="#"><i class="fa fa-twitter-square"></i></a>
-    </div>
-  </aside>
+    <aside class="sidebar-left">
+        <a class="company-logo" href="#">KIW</a>
+        <div class="social-links-top">
+            <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            <a href="#"><i class="fa fa-github-square"></i></a>
+            <a href="#"><i class="fa fa-twitter-square"></i></a>
+        </div>
+        <div class="sidebar-links">
+            <a class="link-blue " href="./index.php"><i class="fa fa-home"></i>Home</a>
+            <a class="link-red selected" href="./resume.php"><i class="fa fa-file-text-o"></i>Resume</a>
+            <a class="link-yellow " href="./portfolio.php"><i class="fa fa-folder-o"></i>Projects</a>
+            <a class="link-green" href="./contact.php"><i class="fa fa-envelope-o"></i>Contact</a>
+        </div>
+        <div class="social-links-bottom">
+            <a href="#"><i class="fa fa-linkedin-square"></i></a>
+            <a href="#"><i class="fa fa-github-square"></i></a>
+            <a href="#"><i class="fa fa-twitter-square"></i></a>
+        </div>
+    </aside>
 
-  <main id="resume-page" class="main-content">
+    <main id="resume-page" class="main-content">
 
-    <div class="section group">
-      <div class="col span_2_of_3">
-        <table id="resume">
+        <div class="section group">
+            <div class="col span_2_of_3">
+                <table id="resume">
 
-          <!-- Employment -->
-          <tr class="section_header">
-            <td colspan="2"><h2>Employment</h2></td>
-          </tr>
+                    <!-- Employment -->
+                    <tr class="section_header">
+                        <td colspan="2"><h2>Employment</h2></td>
+                    </tr>
 
-          <tr class="header_row">
-            <td colspan="2"><h3>Astrophysicist</h3></td>
-          </tr>
-          <tr>
-            <td class="location">Harvard Smithsonian Center for Astrophysics</td>
-            <td class="dates">2011-2015</td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <ul>
-                <li>IT support for the Chandra Space Telescope Director's Office.</li>
-                <li>Develop and maintain websites for various conferences/meetings and symposia sponsored by the CXC, including private registration-statistic pages for admin teams.  Provide in-person technical support during such meetings. </li>
-                <li>Database maintenance, peer review support, in house software writing maintenance, maintenance of web-based target pages.</li>
-                <li>Assist in testing/proof-reading Chandra's web-based tools, webpages, and other deliverables in advance of CfP release. Prepare and maintain/update budgets for successful obsering proposals.</li>
-                <li>Prepare, solicit and analyze input from annual student survey for educators using Chandra data, compile statitics on results</li>
-                <li>Assist and respond to Chandra telescope users through the telescopes helpdesk. Generate reports on Helpdesk</li>
-                <li>Attend astronomical meetings and conferences as a representative of Chandra X-ray Center. Run educational activities related to Chandra science for children of all ages.</li>
-              </ul>
-            </td>
-          </tr>
+                    <tr class="header_row">
+                        <td colspan="2"><h3>Instructor</h3></td>
+                    </tr>
+                    <tr>
+                        <td class="location">Women's Coding Collective</td>
+                        <td class="dates">2015-Present</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <ul>
+                                <!-- One line for responsibilities, two for accomplishments -->
+                                <!-- Responsibilities -->
 
-          <tr class="header_row">
-            <td colspan="2"><h3>Teaching Assistant</h3></td>
-          </tr>
-          <tr>
-            <td class="location">Harvard University, Cambridge, MA</td>
-            <td class="dates">2014</td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <ul>
-                <li>Building Dynamic Web Applications with Laravel. Building applications where users can log-in, create profiles, interact with databases.</li>
-                <li>Hold office hours for troubleshooting/debugging projects with students</li>
-                <li>Teach sections revisting lecture material for interested students</li>
-                <li>Grade ~25 students work throughout the semester, providing feedback and suggestions.</li>
-              </ul>
-            </td>
-          </tr>
+                                <li>Teaching elements of web development to help women excel in the field of web development.</li>
 
-          <tr class="header_row">
-            <td colspan="2"><h3>Teaching Assistant</h3></td>
-          </tr>
-          <tr>
-            <td class="location">Wesleyan University, Middletown, CT</td>
-            <td class="dates">2009-2011</td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <ul>
-                <li>Ran labs, office hours, review sessions for beginning astronomy undergraduates.</li>
-              </ul>
-            </td>
-          </tr>
+                                <li>Answered questions and provided troubleshooting guidance to students through an online forum.</li>
 
-          <!-- Education-->
-          <tr class="section_header">
-            <td colspan="2"><h2>Education</h2></td>
-          </tr>
+                                <li>Offered suggestions on best practices and coding standards.</li>
 
-          <tr class="header_row">
-            <td colspan="2"><h3>Master of Arts: Astronomy</h3></td>
-          </tr>
-          <tr>
-            <td class="location">Wesleyan University, Middletown, CT</td>
-            <td class="dates">May 2011</td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <ul>
-                <li><span class="bold">Thesis</span>: <em>Probing Our Heliospheric History</em>. High-resolution optical spectroscopic analysis of Interstellar Medium absorption in the direction of the Sun’s historical trajectory over the past 40 million years.</li>
-                <li><span class="bold">Research Project</span>: <em>Drifting Sub-Pulses in Radio Pulsars</em> (Completed while working as an intern at the National Radio Astronomy Observatory). Fast Fourier Transform analysis of radio signals from pulsating neutron stars to study underlying vibrational modes in neutron stars to further understand their interior structures.</li>
-              </ul>
-            </td>
-          </tr>
 
-          <tr class="header_row">
-            <td colspan="2"><h3>Bachelor of Science: Physics</h3></td>
-          </tr>
-          <tr>
-            <td class="location">Sonoma State University, Rohnert Park, CA</td>
-            <td class="dates">May 2009</td>
-          </tr>
-          <tr>
-            <td colspan="2">
-              <ul>
+
+                            </ul>
+                        </td>
+                    </tr>
+
+                    <tr class="header_row">
+                        <td colspan="2"><h3>Astrophysicist/Tech Support</h3></td>
+                    </tr>
+                    <tr>
+                        <td class="location">Harvard Smithsonian Center for Astrophysics
+                            <br />Chandra Space Telescope Director's Office <br /></td>
+                        <td class="dates">2011-2015</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <ul>
+                                <!-- One line for responsibilities, two for accomplishments -->
+                                <!-- Responsibilities -->
+                                <!-- <li>Development and maintenance of websites for CXC sponsored conferences, meetings and symposia. Provided technical in-person support during events.</li> -->
+
+                                <li>Maintained database, provided in-house software writing, assisted in testing/proof-reading of web-based tools, webpages, and other documents/deliverables. Provide customer support to observers through help desk system.</li>
+
+                                <!-- <li>Represented Chandra X-ray Center at astronomical meetings. Developed engaging activities to promote education and interest in astronomy for children.</li> -->
+
+                                <!-- Accomplishment -->
+                                <li>Developed online registration process for Chandra X-ray Center-sponsored conferences, and generated private registration statistic pages for admin teams.</li>
+
+                                <!-- Accomplishment -->
+                                <li>Re-designed Chandra X-ray Center meeting websites with responsive design techniques. Updated static HTML tables of astronomical targets observed by Chandra with a modern, interactive JQuery plug-in, added sorting/searching/saving capabilities.</li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                    <tr class="header_row">
+                        <td colspan="2"><h3>Teaching Assistant - Dynamic Web Applications</h3></td>
+                    </tr>
+                    <tr>
+                        <td class="location">Harvard University, Cambridge, MA</td>
+                        <td class="dates">2014</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <ul>
+                                <li>Led weekly section reviewing course material and help students troubleshoot class work.</li>
+                                <li>Graded ~25 students' projects, provided feedback and suggested improvements on code, interface, usability and errors for web applications written in PHP and Laravel.</li>
+                                <li>Provided guidance and suggested troubleshooting techniques through an online forum on server configuration, the basics of version control with Git, program design, code syntax and programming logic, and best practices.</li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                    <tr class="header_row">
+                        <td colspan="2"><h3>Teaching Assistant - Astronomy</h3></td>
+                    </tr>
+                    <tr>
+                        <td class="location">Wesleyan University, Middletown, CT <br /> Sonoma State University, Rohnert Park, CA</td>
+                        <td class="dates">2007-2011</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <ul>
+                                <li>Led students in outdoor astronomy labs. Introduced students to the concepts of telescope design, instructed students on how to use astronomical equipment for public observing and the aquisition of data for astronomical research.</li>
+                                <li>Held office hours for review of lecture material or help with homework assignments.</li>
+                                <li>Ran labs where students repeated some basic yet relevatory scienticif experiments to teach them about the cosmos.</li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                    <!-- Education-->
+                    <tr class="section_header">
+                        <td colspan="2"><h2>Education</h2></td>
+                    </tr>
+
+                    <tr class="header_row">
+                        <td colspan="2"><h3>Master of Arts: Astronomy</h3></td>
+                    </tr>
+                    <tr>
+                        <td class="location">Wesleyan University, Middletown, CT</td>
+                        <td class="dates">May 2011</td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <ul>
+                                <li><span class="bold">Thesis</span>: <em>Probing Our Heliospheric History</em>. High-resolution optical spectroscopic analysis of Interstellar Medium absorption in the direction of the Sun’s historical trajectory over the past 40 million years.</li>
+                                <li><span class="bold">Research Project</span>: <em>Drifting Sub-Pulses in Radio Pulsars</em> (Completed while working as an intern at the National Radio Astronomy Observatory). Fast Fourier Transform analysis of radio signals from pulsating neutron stars to study underlying vibrational modes in neutron stars to further understand their interior structures.</li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                <tr class="header_row">
+                <td colspan="2"><h3>Bachelor of Science: Physics</h3></td>
+                </tr>
+                <tr>
+                <td class="location">Sonoma State University, Rohnert Park, CA</td>
+                <td class="dates">May 2009</td>
+                </tr>
+                <tr>
+                <td colspan="2">
+                <ul>
                 <li><span class="bold">Thesis</span>: <em>Semi-Regular Variable Stars</em>. Observed two SR pulsating stars for two years in multiple wavelengths. Reduced and analyzed all data, found evidence of a previously undetected lag in pulsations between wavelengths.</li>
-              </ul>
-            </td>
-          </tr>
+                </ul>
+                </td>
+                </tr>
 
-          <tr class="header_row">
-            <td colspan="2"><h3>Associate of Arts</h3></td>
-          </tr>
-          <tr>
-            <td class="location">Orange Coast College, Costa Mesa, CA</td>
-            <td class="dates">May 2006</td>
-          </tr>
+                <tr class="header_row">
+                <td colspan="2"><h3>Associate of Arts</h3></td>
+                </tr>
+                <tr>
+                <td class="location">Orange Coast College, Costa Mesa, CA</td>
+                <td class="dates">May 2006</td>
+                </tr>
 
 
-        </table>
-      </div> <!-- close span_2_of_3 -->
+                </table>
+            </div> <!-- close span_2_of_3 -->
 
-      <div id="resume_sidebar" class="col span_1_of_3">
-        <div id="resume_sidebar_content">
-          <!-- TECHNICAL SKILLS-->
-          <h3>Programming Languages</h3>
-          <p>HTML, CSS, Javascript, PHP, Python</p>
+            <div id="resume_sidebar" class="col span_1_of_3">
+                <div id="resume_sidebar_content">
+                    <!-- TECHNICAL SKILLS-->
+                    <h3>Programming Languages</h3>
+                    <p>HTML, CSS, Javascript, PHP, Python</p>
 
-          <h3>Operating Systems</h3>
-          <p>Linux, MacOS</p>
+                    <h3>Operating Systems</h3>
+                    <p>Linux, MacOS</p>
 
-          <h3>Frameworks</h3>
-          <p>Laravel, Wordpress</p>
+                    <h3>Frameworks</h3>
+                    <p>Laravel, Wordpress</p>
 
-          <h3>Misc Technical Skills</h3>
-          <p>GIMP, Microsoft Office, MySQL, Git</p>
+                    <h3>Misc Technical Skills</h3>
+                    <p>GIMP, Microsoft Office Suite, MySQL, Git, Google Docs Suite</p>
 
-          <!-- AWARDS -->
-          <h3>Awards</h3>
-          <p><span class="bold">Chambliss Award</span>: American Astronomical Society award for excellence in graduate research. Received for work at Wesleyan on thesis. </p>
+                    <!-- AWARDS -->
+                    <h3>Awards</h3>
+                    <p><span class="bold">Chambliss Award</span>: American Astronomical Society award for excellence in graduate research. Received for work at Wesleyan on thesis. </p>
 
-          <h3> Outreach/Teaching<h3>
+                    <!-- PUBLICATIONS -->
+                    <h3>Publications</h3>
+                    <p><span class="bold">K. Wyman</span>, S. Redfield, <a href="http://arxiv.org/pdf/1307.2899.pdf" target="_blank">Probing Our Heliospheric History I</a>: High-Resolution Observations of Na I and Ca II Along the Solar Historical Trajectory.</p>
 
-          <!-- PUBLICATIONS -->
-          <h3>Publications</h3>
-          <p><span class="bold">K. Wyman</span>, S. Redfield, <a href="http://arxiv.org/pdf/1307.2899.pdf" target="_blank">Probing Our Heliospheric History I</a>: High-Resolution Observations of Na I and Ca II Along the Solar Historical Trajectory.</p>
-
-        </div> <!-- Close resume_sidebar_content -->
-      </div> <!-- Close span_1_of_3 -->
-    </div> <!-- Close Section group -->
-  </main>
+                </div> <!-- Close resume_sidebar_content -->
+            </div> <!-- Close span_1_of_3 -->
+        </div> <!-- Close Section group -->
+    </main>
 
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="js/plugins.js"></script>
